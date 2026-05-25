7dde73eb39c60ee0b911e16a352880b1fb42ae50
qvosten <xandrstrafexxtopazx@gmail.com>
2026-05-25 20:39:30 +0300

    Добавлена функция showMenu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-int main(){
-    
-    std::cout<< "Hello World" << std::endl;
+void showMenu() {
+    std::cout << "--- MENU ---" << std::endl;
+    std::cout << "1. Start Program" << std::endl;
+    std::cout << "2. Exit" << std::endl;
+}
+
+int main() {
+    std::cout << "Hello World" << std::endl;
     return 0;
 }
