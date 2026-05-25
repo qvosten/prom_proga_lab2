f54af10b970fef8c2d638142087766f640ce0fbc
qvosten <xandrstrafexxtopazx@gmail.com>
2026-05-25 21:01:21 +0300

    Добавлен текст от второго разработчика

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void showMenu() {
 
 int main() {
     std::cout << std::endl; // Отступ для читаемости
+    std::cout << "--- ТУТ БЫЛ РАЗРАБОТЧИК 2 ---" << std::endl;
     showMenu(); 
     return 0;
 }
